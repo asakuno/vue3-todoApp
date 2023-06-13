a88c4488b7df95b220b2413cc702f3ad9c131df4
asakuno <takahiro.asakuno@gmail.com>
2023-06-13 20:43:57 +0900

    [Add]vue-router導入

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+// createRouter メソッドでルーターを生成
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  // routes オプションに指定した path で描画するコンポーネントのルート（route）を定義
+  routes: [
+    {
+      path: '/',  //リクエスパス
+      name: 'home', //このルートの名前
+      component: HomeView  //呼び出されるコンポーネント
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/todo',
+      name: 'todo',
+      component: () => import('../components/TodoApp.vue')
+    },
+  ]
+})
+ 
+export default router
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -7,6 +7,7 @@ export const useCounterStore = defineStore('counter', () => {
   function increment() {
     count.value++
   }
-
-  return { count, doubleCount, increment }
+  const descriment = () => count.value--
+  
+  return { count, doubleCount, increment, descriment }
 })
